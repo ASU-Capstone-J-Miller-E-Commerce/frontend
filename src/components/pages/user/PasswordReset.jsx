b296e91d785d73d3b305df1ee90ed66e3bd3ca1b
Andrew Hejl <hejlandrew@gmail.com>
2025-09-27 11:06:22 -0700

    removed needless line

--- src/components/pages/user/PasswordReset.jsx
+++ src/components/pages/user/PasswordReset.jsx
@@ -19,7 +19,6 @@ export default function PasswordReset () {
                 // reset form
                 receiveResponse(response);
                 reset();
-                setAttachment(null);
         });
     };
 
