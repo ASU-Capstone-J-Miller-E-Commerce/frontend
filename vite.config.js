28377161bbc348cae54c86ab7ca4805f20659777
derekcdee <dcdailey@asu.edu>
2025-10-02 16:54:14 -0700

    color for mobile

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '192.168.1.125', // or true for all interfaces
     port: 3000,
+    strictPort: true // fail instead of auto-picking another port
   }
 })
